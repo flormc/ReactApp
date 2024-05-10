0274527eb514f5a413e5afde93e7408b9dee189e
Florencia <fmartinezcarreras@gmail.com>
2024-05-09 21:12:16 -0300

    Creación de eventos

--- src/App.js
+++ src/App.js
@@ -6,29 +6,41 @@ import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from './CreateTodoButton';
 
 const defaultTodos = [
-  { text: 'Cortar cebolla', completed: true},
-  { text: 'Tomar el curso de Intro a React.js', completed: false},
-  { text: 'Llorar con la llorona', completed: false},
-  { text: 'LALALALALa', completed: false},
-  { text: 'UPS', completed: false},
-]
+  { text: 'Cortar cebolla', completed: true },
+  { text: 'Tomar el Curso de Intro a React.js', completed: false },
+  { text: 'Llorar con la Llorona', completed: false },
+  { text: 'LALALALALA', completed: false },
+  { text: 'Usar estados derivados', completed: true },
+];
 
 function App() {
+  const [todos, setTodos] = React.useState([defaultTodos]);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const totalTodos = todos.length;
+
+  console.log('Los usuarios buscan todos de ' + searchValue);
+
   return (
     <>
-      <TodoCounter completed={16} total={25}/>
-      <TodoSearch />
-
+      <TodoCounter 
+        completed={completedTodos} 
+        total={totalTodos}
+      />
+      <TodoSearch 
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
       <TodoList>
         {defaultTodos.map(todo => (
-        <TodoItem 
-        key={todo.text}
-        text={todo.text}
-        completed={todo.completed}
-        />  
+          <TodoItem 
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+          />  
         ))}
       </TodoList>
-      
       <CreateTodoButton />
     </>
   );
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,7 +2,14 @@ import './TodoItem.css';
 
 function TodoItem(props){
   return(<li className="TodoItem">
-    <span className={`Icon Icon-check ${props.completed&&"Icon-check--active"}`}>✔</span>
-    <p className={`TodoItem-p ${props.completed&&"TodoItem-p--complete"}`}>{props.text}</p>
-    <span className="Icon Icon-delete">❌</span></li>);}
+    <span className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}>
+    ✔
+    </span>
+    <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
+      {props.text}</p>
+    <span className="Icon Icon-delete">
+     ❌
+    </span></li>
+  );
+}
 export{TodoItem};
